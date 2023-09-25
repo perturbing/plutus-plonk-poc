d5c399233879904e2dc15ce2c19d6696085aa032
perturbing <thomas.vellekoop@iohk.io>
2023-09-25 10:30:04 +0200

    pre-inputs to test-vectors and try reading it

--- plutus-plonk/test/Main.hs
+++ plutus-plonk/test/Main.hs
@@ -14,35 +14,60 @@ import Data.ByteString ( pack )
 import Data.Word ()
 import Plutus.Crypto.Plonk.Transcript (getTranscript)
 -- Create a quick type for importing a test vector proof via JSON.
-data Proof = Proof {
-    commitment_a     :: [Integer],
-    commitment_b     :: [Integer],
-    commitment_c     :: [Integer],
-    commitment_z     :: [Integer],
-    t_low            :: [Integer],
-    t_mid            :: [Integer],
-    t_high           :: [Integer],
-    w_omega          :: [Integer],
-    w_omega_zeta     :: [Integer],
-    a_eval           :: [Integer],
-    b_eval           :: [Integer],
-    c_eval           :: [Integer],
-    s_sig1           :: [Integer],
-    s_sig2           :: [Integer],
-    z_omega          :: [Integer]
+data Proof = Proof 
+    { commitment_a     :: [Integer]
+    , commitment_b     :: [Integer]
+    , commitment_c     :: [Integer]
+    , commitment_z     :: [Integer]
+    , t_low            :: [Integer]
+    , t_mid            :: [Integer]
+    , t_high           :: [Integer]
+    , w_omega          :: [Integer]
+    , w_omega_zeta     :: [Integer]
+    , a_eval           :: [Integer]
+    , b_eval           :: [Integer]
+    , c_eval           :: [Integer]
+    , s_sig1           :: [Integer]
+    , s_sig2           :: [Integer]
+    , z_omega          :: [Integer]
 } deriving (Show, Generic)
 
 instance FromJSON Proof 
 instance ToJSON Proof 
 
+data PreIn = PreIn 
+    { n_public         :: Integer                     
+    , power            :: Integer                     
+    , k1               :: [Integer]
+    , k2               :: [Integer]
+    , q_m              :: [Integer]
+    , q_l              :: [Integer]
+    , q_r              :: [Integer] 
+    , q_o              :: [Integer]
+    , q_c              :: [Integer]
+    , s_sig1_pre_in    :: [Integer]
+    , s_sig2_pre_in    :: [Integer]
+    , s_sig3_pre_in    :: [Integer]
+    , x2               :: [Integer]
+    , generator        :: [Integer] 
+} deriving (Show, Generic)
+
+instance FromJSON PreIn 
+instance ToJSON PreIn
+
 convertIntegerG1Point :: [Integer] -> P.BuiltinBLS12_381_G1_Element
 convertIntegerG1Point n = P.bls12_381_G1_uncompress . P.toBuiltin . pack $ Prelude.map fromIntegral n
+
+convertMontgomery :: [Integer] -> Scalar 
+convertMontgomery [a, b, c, d] = mkScalar $ a + b * 2^64 + c * 2^128 + d * 2^192
 
 -- a quick test to see if the serialisation between dummy plonk and plutus bls is the same (it is).
 main :: IO ()
 main = do
-    jsonData <- BL.readFile "test-vectors/proof-test-vector.json"
-    let maybeProof = decode jsonData :: Maybe Proof 
+    jsonDataProof <- BL.readFile "test-vectors/proof-test-vector.json"
+    jsonDataPreIn <- BL.readFile "test-vectors/pre-in-test-vector.json"
+    let maybeProof = decode jsonDataProof :: Maybe Proof 
+    let maybePreIn = decode jsonDataPreIn :: Maybe PreIn
     case maybeProof of
         Just proof -> do let a = convertIntegerG1Point $ commitment_a proof
                          let b = convertIntegerG1Point $ commitment_b proof
@@ -61,7 +86,8 @@ main = do
                          let wOmegaZeta = convertIntegerG1Point $ w_omega_zeta proof
                          let (beta, gamma, alpha, zeta, v, u) = getTranscript a b c z tLow tMid tHigh aEval bEval cEval sSig1 sSig2 zOmega wOmega wOmegaZeta
                          print u
-        Nothing -> putStrLn "Failed to parse JSON."
-
-convertMontgomery :: [Integer] -> Scalar 
-convertMontgomery [a, b, c, d] = mkScalar $ a + b * 2^64 + c * 2^128 + d * 2^192
+                         case maybePreIn of
+                            -- test is the generator is indeed the correct generator as in the dummy plonk implementation
+                            Just preIn -> print $ convertMontgomery (generator preIn)
+                            Nothing -> putStrLn "Failed to parse preIn JSON"
+        Nothing -> putStrLn "Failed to parse proof JSON."
